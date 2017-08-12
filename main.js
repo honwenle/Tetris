e7236052d943e87bf05560a15b793b509949aee6
洪文乐 <noclip@foxmail.com>
2017-08-12 09:25:14 +0800

    7种

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 var ctx = document.getElementById('can').getContext('2d');
 var isPause = false;
 var freezeList = [],
-    nowList = {},
-    nextList = {},
+    nowList = [],
+    nextList = [],
     dir = 0;
 var typeList = [
-    {
-        arr: [44, 45, 64, 65],
-        color: '#0ff'
-    }
+    [44, 45, 64, 65], // O
+    [64, 44, 24 ,4], // I
+    [64, 65, 45, 46], // S
+    [64, 65, 44, 43], // Z
+    [64, 65, 44, 24], // L
+    [64, 65, 45, 25], // J
+    [64, 44, 43, 45] // T
 ];
+var colorList = ['#0ff','#0f0', '#f0f', '#60f', '#f60', '#f00', '#ff0'],
+    nowIndex = 0;
 function getID (x, y) {}
 function getXY (id) {
     return {
@@ -18,41 +23,45 @@ function getXY (id) {
     }
 }
 function draw (obj, color) {
-    ctx.fillStyle = color || obj.color;
-    obj.arr.forEach(function(n) {
+    ctx.fillStyle = color || colorList[nowIndex];
+    obj.forEach(function(n) {
         var xy = getXY(n);
         ctx.fillRect(xy.x, xy.y, 18, 18);
     });
 }
 function newList () {
-    nowList = typeList[0];
+    nowIndex = Math.floor(Math.random() * colorList.length);
+    nowList = typeList[nowIndex];
 }
 function move () {
     if (isPause) {
         return false;
     }
-    nextList.color = nowList.color;
-    nextList.arr = nowList.arr.map(function (n) {
+    nextList = nowList.map(function (n) {
         return n + dir;
     });
     var canMove = true, canFreeze = false;
-    nextList.arr.forEach(function (n) {
+    nextList.forEach(function (n) {
         if (dir==-1 && n%20==19 || dir==1 && n%20==10) {
             canMove = false;
         }
-        if (n > 499 || freezeList.indexOf(n) > -1) {
+        if ((dir==-1 || dir==1) && freezeList.indexOf(n) > -1) {
+            canMove = false;
+        } else if (n > 499 || dir==20 && freezeList.indexOf(n) > -1) {
             canFreeze = true;
         }
     });
     if (canFreeze) {
-        freezeList = freezeList.concat(nowList.arr);
+        freezeList = freezeList.concat(nowList);
+        // TODO: 检查、消除
+        // TODO: 判断死亡
         newList();
         return false;
     }
     if (canMove) {
         draw(nowList, '#000');
         nowList = nextList;
-        nextList = {};
+        nextList = [];
         draw(nowList);
     }
 }
