187301451ec9241ccc921dd932b67e4b140ca5b2
洪文乐 <noclip@foxmail.com>
2017-08-14 11:44:03 +0800

    pow

--- main.js
+++ main.js
@@ -135,7 +135,7 @@ function move () {
 function showScore () {
     score += scoreList[clearRows.length];
     lv = ~~(score / 1000);
-    speed = speed * Math.sqrt(.8, lv);
+    speed = speed * Math.pow(.8, lv);
     ctx.clearRect(0, 0, 200, 18);
     ctx.font = '18px 微软雅黑';
     ctx.fillStyle = '#fff';
